f8766acc65d49b8f1bc9d2ee6d92163d07bb145d
ases573 <ases573@gmail.com>
2017-06-26 20:56:52 +0530

    "[GIT AUTO COMMIT]: Mon Jun 26 2017 20:56:52 GMT+0530 (India Standard Time)"

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,82 @@
-import React , {Component} from 'react';
+import React, {Component} from 'react';
+import {firebaseApp} from '../firebase';
+class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            error: {
+                message: ''
+            }
+        }
+    }
+    signIn()
+    {
+        console.log('this.state', this.state);
+        const email = this.state.email;
+        const password = this.state.password;
+        firebaseApp
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .catch(error => {
+                console.log("Error:",error);
+                this.setState({error})
+            });
 
-class SignIn extends Component{
-    render(){
-        return(
-        <div>SignIn from jsx</div>)
+    }
+    render() {
+        return (
+            <div className="card">
+                <div className="card-block">
+                    <hr/>
+                    <div className="form-header text-center">
+                        <h3>
+                            <i className="fa fa-user"></i>
+                            Register:</h3>
+                    </div>
+                    <hr/>
+                    <form id="signInForm">
+                        <div className="md-form">
+                            <i className="fa  fa-envelope-open prefix"></i>
+                            <input
+                                type="text"
+                                id="form2"
+                                className="form-control"
+                                placeholder="E-mail"
+                                onChange={event => this.setState({email: event.target.value})}
+                                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+                                title="Use valid email ID"
+                                required/>
+
+                        </div>
+                        <hr/>
+                        <div className="md-form">
+                            <i className="fa fa-lock prefix"></i>
+                            <input
+                                type="password"
+                                id="form4"
+                                className="form-control"
+                                placeholder="Password"
+                                onChange={event => this.setState({password: event.target.value})}
+                                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                                title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+                                required/>
+
+                        </div>
+
+                        <div className="text-center">
+                            <button className="btn btn-indigo" type="button" onClick={() => this.signIn()}>Sign up</button>
+                            <hr/>
+
+                        </div>
+                    </form>
+                    <div className="well">{this.state.error.message}</div>
+
+                </div>
+            </div>
+
+        )
     }
 }
 
