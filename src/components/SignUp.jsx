20e71a69b0895d1c38ea78917ab9edd34cbabeb2
ases573 <ases573@gmail.com>
2017-06-26 20:18:52 +0530

    "[GIT AUTO COMMIT]: Mon Jun 26 2017 20:18:52 GMT+0530 (India Standard Time)"

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -4,11 +4,14 @@ class SignUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email :'',
-            password : ''
+            email: '',
+            password: ''
         }
     }
-    
+    signUp()
+    {
+        console.log('this.state', this.state);
+    }
     render() {
         return (
             <div className="card">
@@ -22,18 +25,28 @@ class SignUp extends Component {
 
                     <div className="md-form">
                         <i className="fa  fa-envelope-open prefix"></i>
-                        <input type="text" id="form2" className="form-control" placeholder="E-mail" onChange={event => this.setState({email:event.target.value})}/>
+                        <input
+                            type="text"
+                            id="form2"
+                            className="form-control"
+                            placeholder="E-mail"
+                            onChange={event => this.setState({email: event.target.value})}/>
 
                     </div>
 
                     <div className="md-form">
                         <i className="fa fa-lock prefix"></i>
-                        <input type="password" id="form4" className="form-control" placeholder="Password" onChange={event => this.setState({password:event.target.value})}/>
+                        <input
+                            type="password"
+                            id="form4"
+                            className="form-control"
+                            placeholder="Password"
+                            onChange={event => this.setState({password: event.target.value})}/>
 
                     </div>
 
                     <div className="text-center">
-                        <button className="btn btn-indigo">Sign up</button>
+                        <button className="btn btn-indigo" onClick={() => this.signUp()}>Sign up</button>
                         <hr/>
 
                     </div>
