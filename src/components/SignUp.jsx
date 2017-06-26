27c6ae7d1dd616af269ba2d4e414aa67d93734fe
ases573 <ases573@gmail.com>
2017-06-26 20:00:52 +0530

    "[GIT AUTO COMMIT]: Mon Jun 26 2017 20:00:52 GMT+0530 (India Standard Time)"

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,39 +1,39 @@
-import React , {Component} from 'react';
+import React, {Component} from 'react';
 
-class SignUp extends Component{
-    render(){
-        return(      
-<div className="card">
-    <div className="card-block">
+class SignUp extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card-block">
 
-        
-        <div className="form-header text-center">
-            <h3><i className="fa fa-user"></i> Register:</h3>
-        </div>
+                    <div className="form-header text-center">
+                        <h3>
+                            <i className="fa fa-user"></i>
+                            Register:</h3>
+                    </div>
 
-        <div className="md-form">
-            <i className="fa fa-envelope prefix"></i>
-            <input type="text" id="form2" className="form-control" />
-            
-        </div>
+                    <div className="md-form">
+                        <i className="fa fa-envelope prefix"></i>
+                        <input type="text" id="form2" className="form-control"/>
 
-        <div className="md-form">
-            <i className="fa fa-lock prefix"></i>
-            <input type="password" id="form4" className="form-control" />
-            
-        </div>
+                    </div>
 
-        <div className="text-center">
-            <button className="btn btn-indigo">Sign up</button>
-            <hr />
-           
-        </div>
+                    <div className="md-form">
+                        <i className="fa fa-lock prefix"></i>
+                        <input type="password" id="form4" className="form-control"/>
 
-    </div>
-</div>
+                    </div>
 
+                    <div className="text-center">
+                        <button className="btn btn-indigo">Sign up</button>
+                        <hr/>
 
-)
+                    </div>
+
+                </div>
+            </div>
+
+        )
     }
 }
 
