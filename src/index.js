516a5a05ef166a388e6bacca467237b9687455a9
ases573 <ases573@gmail.com>
2017-06-26 19:24:52 +0530

    "[GIT AUTO COMMIT]: Mon Jun 26 2017 19:24:52 GMT+0530 (India Standard Time)"

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render( <Router path='/' history={browserHistory}>
+    
+    </Router> , document.getElementById('root'));
