aa52546a577ea4a8827026a7ab2a578f20dd68b1
ases573 <ases573@gmail.com>
2017-06-26 20:28:52 +0530

    "[GIT AUTO COMMIT]: Mon Jun 26 2017 20:28:52 GMT+0530 (India Standard Time)"

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -23,41 +23,41 @@ class SignUp extends Component {
                             Register:</h3>
                     </div>
                     <hr/>
-                     <form id="signUpForm">
-                    <div className="md-form">
-                        <i className="fa  fa-envelope-open prefix"></i>
-                        <input
-                            type="text"
-                            id="form2"
-                            className="form-control"
-                            placeholder="E-mail"
-                            onChange={event => this.setState({email: event.target.value})}
-                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
-                            title="Use valid email ID"
-                            required/>
+                    <form id="signUpForm">
+                        <div className="md-form">
+                            <i className="fa  fa-envelope-open prefix"></i>
+                            <input
+                                type="text"
+                                id="form2"
+                                className="form-control"
+                                placeholder="E-mail"
+                                onChange={event => this.setState({email: event.target.value})}
+                                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+                                title="Use valid email ID"
+                                required/>
 
-                    </div>
-                    <hr/>
-                    <div className="md-form">
-                        <i className="fa fa-lock prefix"></i>
-                        <input
-                            type="password"
-                            id="form4"
-                            className="form-control"
-                            placeholder="Password"
-                            onChange={event => this.setState({password: event.target.value})}
-                            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                            title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
-                            required/>
-
-                    </div>
-
-                    <div className="text-center">
-                        <button className="btn btn-indigo" onClick={() => this.signUp()}>Sign up</button>
+                        </div>
                         <hr/>
+                        <div className="md-form">
+                            <i className="fa fa-lock prefix"></i>
+                            <input
+                                type="password"
+                                id="form4"
+                                className="form-control"
+                                placeholder="Password"
+                                onChange={event => this.setState({password: event.target.value})}
+                                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                                title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+                                required/>
 
-                    </div>
+                        </div>
 
+                        <div className="text-center">
+                            <button className="btn btn-indigo" type="button" onClick={() => this.signUp()}>Sign up</button>
+                            <hr/>
+
+                        </div>
+                    </form>
                 </div>
             </div>
 
