a5c588109fa4adc19eb5fc27bae1cf29777d27b4
ases573 <ases573@gmail.com>
2017-06-27 16:32:48 +0530

    "[GIT AUTO COMMIT]: Tue Jun 27 2017 16:32:48 GMT+0530 (India Standard Time)"

--- src/components/GoalList.jsx
+++ src/components/GoalList.jsx
@@ -4,12 +4,16 @@ import {goalRef} from '../firebase';
 class GoalList extends Component {
     componentDidMount() {
         goalRef.on('value', snap => {
+            let goals = [];
             snap.forEach(goal => {
                 let goalObject = goal.val();
                 console.log("goalObject", goalObject);
+                goals.push(goalObject);
             })
+            console.log("GOALS",goals);
         })
     }
+
     render() {
         return (
             <div>GoalList</div>
