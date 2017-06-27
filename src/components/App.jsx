6c98527d59636414be54faa1f1fe58e260e28327
ases573 <ases573@gmail.com>
2017-06-27 11:58:47 +0530

    "[GIT AUTO COMMIT]: Tue Jun 27 2017 11:58:47 GMT+0530 (India Standard Time)"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,8 @@ class App extends Component {
         return (
             <div className="text-center">
                 <h3 className="text-center">Goals</h3>
-                <
+                <div> Add Goals </div>
+                <div> Goal List </div>
                 <button className="btn btn-danger" onClick={() => this.signOut()}>Sign Out</button>
             </div>
         )
