87523f608e5a4282190837d6ffea045d22c8c98c
ases573 <ases573@gmail.com>
2017-06-27 12:04:47 +0530

    "[GIT AUTO COMMIT]: Tue Jun 27 2017 12:04:47 GMT+0530 (India Standard Time)"

--- src/components/AddGoal.jsx
+++ src/components/AddGoal.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class AddGoal extends Component {
     constructor(props) {
         super(props);
-        this.state{
+        this.state = {
             title : ''
         }
     }
@@ -14,7 +14,8 @@ class AddGoal extends Component {
 					<div className="col-md-4 col-md-offset-3">
 
 						<div className="form-group has-feedback">
-<input type="text" className="form-control text-center" placeholder="Enter Goals" required onChange={event => this.setState({ query: event.target.value })} onKeyPress={event => { if (event.key === 'Enter') this.search() } } />
+<input type="text" className="form-control text-center" placeholder="Enter Goals" required onChange={event => this.setState({ title: event.target.value })} onKeyPress={event => { if (event.key === 'Enter') this.AddGoal() } } />
+                        <button className="btn btn-success" onClick={() => this.AddGoal()}>Add Goal</button>
 
 						</div>
 
