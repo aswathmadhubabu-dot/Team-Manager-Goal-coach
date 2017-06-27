389c44f6236122df130d63375a5842f53d6986d2
ases573 <ases573@gmail.com>
2017-06-27 11:22:47 +0530

    "[GIT AUTO COMMIT]: Tue Jun 27 2017 11:22:47 GMT+0530 (India Standard Time)"

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {Router, Route, browserHistory} from 'react-router';
 import {firebaseApp} from './firebase.js';
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
-firebaseApp.auth().onAuthStateChanged(user => {
-    if(user)
-    {
-        console.log('user has signed in or signed out',user);
-        browserHistory.push('/app');
-    }
-    else
-    {
-        console.log('signes out');
-        browserHistory.replace('/signin');
-    }
-})
+const store = createStore();
+firebaseApp
+    .auth()
+    .onAuthStateChanged(user => {
+        if (user) {
+            console.log('user has signed in or signed out', user);
+            browserHistory.push('/app');
+        } else {
+            console.log('signes out');
+            browserHistory.replace('/signin');
+        }
+    })
 
-ReactDOM.render( <Router path='/' history={browserHistory}>
-    <Route path='/app' component={App} />
-    <Route path='/signin' component={SignIn} />
-    <Route path='/signup' component={SignUp} />
-    </Router> , document.getElementById('root'));
+ReactDOM.render(
+    <Provider store ={store}>
+    <Router path='/' history={browserHistory}>
+        <Route path='/app' component={App}/>
+        <Route path='/signin' component={SignIn}/>
+        <Route path='/signup' component={SignUp}/>
+    </Router>
+</Provider>, document.getElementById('root'));
