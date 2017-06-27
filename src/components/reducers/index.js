11bde50b86116d053eb79c709792c50303abb391
ases573 <ases573@gmail.com>
2017-06-27 11:34:47 +0530

    "[GIT AUTO COMMIT]: Tue Jun 27 2017 11:34:47 GMT+0530 (India Standard Time)"

---
+++ src/components/reducers/index.js
@@ -0,0 +1,14 @@
+import {SIGNED_IN} from '../constants.js';
+
+let user = {
+    email :null
+}
+
+export default (state = user,action) => {
+    switch(action.type)
+    {
+        case SIGNED_IN :
+        const {email} = action;
+        user = {email};
+    }
+}
