fa18423374ed2eaa5653bd1c2a34ff3781830b33
ases573 <ases573@gmail.com>
2017-06-27 12:16:47 +0530

    "[GIT AUTO COMMIT]: Tue Jun 27 2017 12:16:47 GMT+0530 (India Standard Time)"

--- src/components/AddGoal.jsx
+++ src/components/AddGoal.jsx
@@ -4,28 +4,39 @@ class AddGoal extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            title : ''
+            title: ''
         }
     }
     AddGoal()
     {
-        console.log("Goal State",this.state);
-    }    
-    render(){
-        return(<div>
-            <div className="container">
-					<div className="col-md-4 col-md-offset-4">
+        console.log("Goal State", this.state);
+    }
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="col-md-4 col-md-offset-4">
 
-						<div className="form-group has-feedback">
-<input type="text" className="form-control text-center" placeholder="Enter Goals" required onChange={event => this.setState({ title: event.target.value })} onKeyPress={event => { if (event.key === 'Enter') this.AddGoal() } } />
-                        <button className="btn btn-success" onClick={() => this.AddGoal()}>Add Goal</button>
+                        <div className="form-group has-feedback">
+                            <input
+                                type="text"
+                                className="form-control text-center"
+                                placeholder="Enter Goals"
+                                required
+                                onChange={event => this.setState({title: event.target.value})}
+                                onKeyPress={event => {
+                                if (event.key === 'Enter') 
+                                    this.AddGoal()
+                            }}/>
+                            <button className="btn btn-success" onClick={() => this.AddGoal()}>Add Goal</button>
 
-						</div>
+                        </div>
 
-					</div>
-				</div>
-        </div> )
-        
+                    </div>
+                </div>
+            </div>
+        )
+
     }
 }
 
