9eeab813259dcd5408cf92a35364c25f273180cc
ases573 <ases573@gmail.com>
2017-06-28 19:07:55 +0530

    "[GIT AUTO COMMIT]: Wed Jun 28 2017 19:07:55 GMT+0530 (India Standard Time)"

--- src/components/GoalItem.jsx
+++ src/components/GoalItem.jsx
@@ -11,31 +11,13 @@ class GoalItem extends Component {
     render() {
  const {email,title} = this.props.goal;
         return (
-            <div className="col-lg-6 col-lg-offset-3 container text-center well">
-                <ul className="list-group">
-                    {this
-                        .props
-                        .goals
-                        .map(function (goal, index) {
-                            return (
-                                <li key={index} className="list-group-item">{goal.title}&nbsp;&nbsp;<span>Submitted By :<em>{goal.email}
-                                        </em>
-                                    </span>
-                                    <button className="btn btn-primary" onClick={() => this.completeGoal()}>Complete</button>
-                                </li>
-                            )
-                        })
-}
-                </ul>
-            </div>
+           <div>
+               <strong>{title}</strong>
+               <span>Submitted By:<em> {email} </em></span>
+           </div>
         )
     }
 }
 
-function mapStateToProps(state) {
-    const {goals} = state;
-    console.log('state in goalList', goals);
-    return {goals}
-}
 
-export default connect(mapStateToProps, {setGoals})(GoalList);
+export default GoalItem;
