bb10ca6b5f847a39a105bb9a6693d5c01a6883ef
ases573 <ases573@gmail.com>
2017-06-28 20:17:55 +0530

    "[GIT AUTO COMMIT]: Wed Jun 28 2017 20:17:55 GMT+0530 (India Standard Time)"

--- src/components/CompleteGoalList.jsx
+++ src/components/CompleteGoalList.jsx
@@ -1,29 +1,44 @@
 import React, {Component} from 'react';
 import {completeGoalRef} from '../firebase';
-import {setCompleted}  from '../actions';
+import {setCompleted} from '../actions';
 class CompleteGoalList extends Component {
-    componentDidMount(){
+    componentDidMount() {
         completeGoalRef.on('value', snap => {
-            let completeGoals = [];  
-             snap.forEach(completeGoal => {
+            let completeGoals = [];
+            snap.forEach(completeGoal => {
                 const {email, title} = completeGoal.val();
                 goals.push({email, title});
             })
-            console.log("COMPLETED GOALS",completeGoals);
-            this.props.setCompleted(completeGoals);
+            console.log("COMPLETED GOALS", completeGoals);
+            this
+                .props
+                .setCompleted(completeGoals);
         })
     }
     render() {
         return (
-            <div>
-                Complete Goal List
+           <div className="col-lg-6 col-lg-offset-3 container text-center well">
+                <ul className="list-group">
+                    {this
+                        .props
+                        .completeGoals
+                        .map(function (completeGoal, index) {
+                            return (
+                                <li key={index} className="list-group-item">
+                                    {goal.title}&nbsp;&nbsp;
+                                    <GoalItem key={index} goal={goal}/>
+                                </li>
+                            )
+                        })
+}
+                </ul>
             </div>
         );
     }
 }
 function mapStateToProps(state) {
-    const {goals} = state;
-    console.log('state in goalList', goals);
-    return {goals}
+    const {completeGoals} = state;
+    console.log('state in CompletegoalList', completeGoals);
+    return {completeGoals}
 }
 export default CompleteGoalList;
