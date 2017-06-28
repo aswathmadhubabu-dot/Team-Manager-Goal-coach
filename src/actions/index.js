d1da0d18704259782a6dd201521bf94535181b66
ases573 <ases573@gmail.com>
2017-06-28 20:09:55 +0530

    "[GIT AUTO COMMIT]: Wed Jun 28 2017 20:09:55 GMT+0530 (India Standard Time)"

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import {SIGNED_IN} from '../constants.js';
 import {SET_GOALS} from '../constants.js';
+import {SET_COMPLETED} from '../constants.js'
 
 export function logUser(email) {
     const action = {
@@ -12,6 +13,14 @@ export function logUser(email) {
 export function setGoals(goals) {
     const action = {
         type: SET_GOALS,
+        goals
+    }
+    return action;
+}
+
+export function setCompleted(goals) {
+    const action = {
+        type: SET_COMPLETED,
         goals
     }
     return action;
