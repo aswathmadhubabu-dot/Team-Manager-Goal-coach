fea8c0c2f9594839e5a96e423acc5885278de4b9
ases573 <ases573@gmail.com>
2017-06-28 19:09:55 +0530

    "[GIT AUTO COMMIT]: Wed Jun 28 2017 19:09:55 GMT+0530 (India Standard Time)"

--- src/components/GoalItem.jsx
+++ src/components/GoalItem.jsx
@@ -1,23 +1,20 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {completeGoalRef} from '../firebase';
+// import {connect} from 'react-redux'; import {completeGoalRef} from
+// '../firebase';
 class GoalItem extends Component {
-   
-    completeGoal()
-    {
-        
-    }
 
     render() {
- const {email,title} = this.props.goal;
+        const {email, title} = this.props.goal;
         return (
-           <div>
-               <strong>{title}</strong>
-               <span>Submitted By:<em> {email} </em></span>
-           </div>
+            <div>
+                <strong>{title}</strong>
+                <span>Submitted By:<em>
+                        {email}
+                    </em>
+                </span>
+            </div>
         )
     }
 }
-
 
 export default GoalItem;
