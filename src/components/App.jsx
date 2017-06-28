431f1cff2f6974b24ed7d5b1f1419560dacc18b5
ases573 <ases573@gmail.com>
2017-06-28 20:03:55 +0530

    "[GIT AUTO COMMIT]: Wed Jun 28 2017 20:03:55 GMT+0530 (India Standard Time)"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {firebaseApp} from '../firebase';
 import AddGoal from './AddGoal';
 import GoalList from './GoalList';
+import CompleteGoalList from './CompleteGoalList';
 class App extends Component {
     signOut() {
         firebaseApp
@@ -14,8 +15,12 @@ class App extends Component {
             <div className="text-center">
                 <h3 className="text-center">Goal Coach</h3>
                 <AddGoal />
+                <hr />
                 <h4 className="text-center">Goals</h4>
                 <GoalList />
+                <hr />
+                <h4 className="text-center">Complete Goals List</h4>
+                <CompleteGoalList />
                 <button className="btn btn-danger" onClick={() => this.signOut()}>Sign Out</button>
             </div>
         )
