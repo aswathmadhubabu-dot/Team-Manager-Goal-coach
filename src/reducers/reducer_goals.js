5004a68011cd09d1f2b437e230dd5772bb28fa50
ases573 <ases573@gmail.com>
2017-06-28 17:47:55 +0530

    "[GIT AUTO COMMIT]: Wed Jun 28 2017 17:47:55 GMT+0530 (India Standard Time)"

---
+++ src/reducers/reducer_goals.js
@@ -0,0 +1,13 @@
+import {SET_GOALS} from '../constants.js';
+
+export default(state = user, action) => {
+    switch (action.type) {
+        case SET_GOALS:
+            const {goals} = action;
+
+            return goals;
+        default:
+            return state;
+
+    }
+}
