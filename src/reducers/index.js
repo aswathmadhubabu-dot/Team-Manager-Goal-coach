153a5d5b0c219898f1b472d608a648d5a74e605b
ases573 <ases573@gmail.com>
2017-06-28 20:11:55 +0530

    "[GIT AUTO COMMIT]: Wed Jun 28 2017 20:11:55 GMT+0530 (India Standard Time)"

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import {combineReducers} from 'redux';
 import user from './reducer_user';
 import goals from './reducer_goals';
-export default combineReducers({user, goals})
+import completeGoals from './reducer_completed_goals';
+export default combineReducers({user, goals, completeGoals});
