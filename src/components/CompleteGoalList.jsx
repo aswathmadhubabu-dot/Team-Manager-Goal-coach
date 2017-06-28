24b23a2521d44269c38a72ff0467b7c54080682e
ases573 <ases573@gmail.com>
2017-06-28 20:05:55 +0530

    "[GIT AUTO COMMIT]: Wed Jun 28 2017 20:05:55 GMT+0530 (India Standard Time)"

--- src/components/CompleteGoalList.jsx
+++ src/components/CompleteGoalList.jsx
@@ -1,13 +1,17 @@
-import React,{Component} from 'react';
-
-class CompleteGoalList extends Component{
-render() {
-    return (
-        <div>
-            Complete Goal List
-        </div>
-    );
-}
+import React, {Component} from 'react';
+import {completeGoalRef} from '../firebase';
+class CompleteGoalList extends Component {
+    componentDidMount(){
+        completeGoalRef.on('value', snap => {
+            let completeGoals = [];  
+    }
+    render() {
+        return (
+            <div>
+                Complete Goal List
+            </div>
+        );
+    }
 }
 
 export default CompleteGoalList;
