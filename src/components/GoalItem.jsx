8b28bce34e342740c0b78827bb0fdda3273e8de5
ases573 <ases573@gmail.com>
2017-06-28 18:51:55 +0530

    "[GIT AUTO COMMIT]: Wed Jun 28 2017 18:51:55 GMT+0530 (India Standard Time)"

---
+++ src/components/GoalItem.jsx
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {completeGoalRef} from '../firebase';
+class GoalItem extends Component {
+    componentDidMount() {
+        goalRef.on('value', snap => {
+            let goals = [];
+            snap.forEach(goal => {
+                const {email, title} = goal.val();
+                goals.push({email, title});
+            })
+            console.log("GOALS", goals);
+            this
+                .props
+                .setGoals(goals);
+        })
+
+    }
+    completeGoal()
+    {
+        
+    }
+
+    render() {
+
+        return (
+            <div className="col-lg-6 col-lg-offset-3 container text-center well">
+                <ul className="list-group">
+                    {this
+                        .props
+                        .goals
+                        .map(function (goal, index) {
+                            return (
+                                <li key={index} className="list-group-item">{goal.title}&nbsp;&nbsp;<span>Submitted By :<em>{goal.email}
+                                        </em>
+                                    </span>
+                                    <button className="btn btn-primary" onClick={() => this.completeGoal()}>Complete</button>
+                                </li>
+                            )
+                        })
+}
+                </ul>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    const {goals} = state;
+    console.log('state in goalList', goals);
+    return {goals}
+}
+
+export default connect(mapStateToProps, {setGoals})(GoalList);
