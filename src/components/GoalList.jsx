75c2f9e6da71cce3fb13c3f5ad693ab50053d5de
ases573 <ases573@gmail.com>
2017-06-28 18:41:55 +0530

    "[GIT AUTO COMMIT]: Wed Jun 28 2017 18:41:55 GMT+0530 (India Standard Time)"

--- src/components/GoalList.jsx
+++ src/components/GoalList.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {setGoals} from '../actions';
 import {goalRef} from '../firebase';
-
+import {completeGoalRef} from '../firebase';
 class GoalList extends Component {
     componentDidMount() {
         goalRef.on('value', snap => {
