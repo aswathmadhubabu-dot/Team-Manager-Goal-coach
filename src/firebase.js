f6b4951cca01923f218205894b2d2a82b8cf56cd
Aswath <ases573@gmail.com>
2020-10-23 08:46:30 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,7 @@
 import * as firebase from 'firebase';
 
  const config = {
-    apiKey: "AIzaSyDpSZ90AKUhZrwhgGEW90vXculp6QFxbDM",
-    authDomain: "goalcoach-ae743.firebaseapp.com",
-    databaseURL: "https://goalcoach-ae743.firebaseio.com",      
-    projectId: "goalcoach-ae743",
-    storageBucket: "goalcoach-ae743.appspot.com",
-    messagingSenderId: "183303894280"
+    
   };
 
   export const firebaseApp = firebase.initializeApp(config);
